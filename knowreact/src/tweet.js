612cbbd5d3fb444186b0fae7e9a1f2e98dc9df10
Anannya Mahapatra <anannya1947@gmail.com>
2021-09-09 15:12:59 +0530

    ES6 react

--- knowreact/src/tweet.js
+++ knowreact/src/tweet.js
@@ -1,12 +1,12 @@
 import React from "react";
 import "./App.css"
 
-function Tweet(props){
+function Tweet({name, likes, message}){
     return(
         <div className="tweet">
-            <h3>{props.name}</h3>
-            <p>{props.message}</p>
-            <h3>Likes: {props.likes}</h3>
+            <h3>{name}</h3>
+            <p>{message}</p>
+            <h3>Likes: {likes}</h3>
         </div>
     );
 }
