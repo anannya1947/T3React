97b4184b31c010cb80da50ac4ff6ca3e44f86fe5
Anannya Mahapatra <anannya1947@gmail.com>
2021-09-09 13:25:07 +0530

    empty files

--- knowreact/src/App.js
+++ knowreact/src/App.js
@@ -1,12 +1,10 @@
 import React from "react";
-import Hello from './sayHello';
 
 function App(){
   
   return (
     <div>
-      <h1>This is the App Component</h1>
-      <Hello />
+      
     </div>
   );
 }
