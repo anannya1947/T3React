99fbb0997ff226ec87dc8db043f4c39e0c280142
Anannya Mahapatra <anannya1947@gmail.com>
2021-09-09 14:40:56 +0530

    replicating a tweet structure

--- knowreact/src/App.js
+++ knowreact/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Tweet from "./tweet";
 
 function App(){
   
   return (
     <div>
-      
+      <h1>Hello React</h1>
+      <Tweet />
     </div>
   );
 }
---
+++ knowreact/src/tweet.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Tweet(){
+    return(
+        <div>
+            <h3>Name</h3>
+            <p>A random Tweet</p>
+            <h3>No. of Likes</h3>
+        </div>
+    );
+}
+
+export default Tweet;
