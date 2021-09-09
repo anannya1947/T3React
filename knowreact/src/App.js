6b8cc1d6b09c3656dc16fe72a9d520c963dfb70f
Anannya Mahapatra <anannya1947@gmail.com>
2021-09-09 14:59:51 +0530

    added some CSS

--- knowreact/src/App.js
+++ knowreact/src/App.js
@@ -4,8 +4,10 @@ import Tweet from "./tweet";
 function App(){
   
   return (
-    <div>
-      <h1>Hello React</h1>
+    <div className="app">
+      <Tweet />
+      <Tweet />
+      <Tweet />
       <Tweet />
     </div>
   );
