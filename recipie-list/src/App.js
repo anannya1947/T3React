24669b1c68b3c4bd31eae4d5dbb6f50e5de67ee4
Anannya Mahapatra <anannya1947@gmail.com>
2021-09-12 15:01:14 +0530

    a search bar is added

--- recipie-list/src/App.js
+++ recipie-list/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import react from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+
+  const APP_ID = "20b3d753";
+  const APP_KEY = "05e84cda3944b64dad730cb86f1ff7ac";
+  const example = `https://api.edamam.com/api/recipes/v2/0123456789abcdef0123456789abcdef?app_id=APP_ID&app_key=APP_KEY&type=public`;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form className="search-form">
+        <input className="search-bar" type="text" />
+        <button className="search-button" type="submit">Search</button>
+      </form>
     </div>
   );
 }
