100b1ab78162ab6c8c23ff304d1060bc812e5c36
Anannya Mahapatra <anannya1947@gmail.com>
2021-09-13 12:24:04 +0530

    display recipe list

--- recipie-list/src/App.js
+++ recipie-list/src/App.js
@@ -1,4 +1,5 @@
 import react,{useEffect, useState} from 'react';
+import Recipe from './Recipe';
 import './App.css';
 
 const App = () => {
@@ -7,26 +8,32 @@ const App = () => {
   const APP_ID = "20b3d753";
   const APP_KEY = "05e84cda3944b64dad730cb86f1ff7ac";
 
-  const [recipe, setRecipe] = useState([]);
+  const [recipes, setRecipes] = useState([]);
 
   //reloads content of the page at a specific instant
   useEffect(() =>{
-    getRecipies();
+    getRecipes();
   }, []);
 
   //Makes API call 
-  const getRecipies = async () =>{
+  const getRecipes = async () =>{
     const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
     const data = await response.json();
-    setRecipe(data.hits);
+    setRecipes(data.hits);
+    console.log(data.hits);
   };
 
   return (
     <div className="App">
+
       <form className="search-form">
         <input className="search-bar" type="text" />
         <button className="search-button" type="submit">Search</button>
       </form>
+
+      {recipes.map(recipe => (
+        <Recipe />
+      ))}
     </div>
   );
 }
