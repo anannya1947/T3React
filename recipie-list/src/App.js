7e4cb8aa25a9c3507898af54ce6571dc2498a589
Anannya Mahapatra <anannya1947@gmail.com>
2021-09-13 11:57:45 +0530

    API call

--- recipie-list/src/App.js
+++ recipie-list/src/App.js
@@ -1,11 +1,23 @@
-import react from 'react';
+import react,{useEffect, useState} from 'react';
 import './App.css';
 
 const App = () => {
 
+  //edamam API authorization identities
   const APP_ID = "20b3d753";
   const APP_KEY = "05e84cda3944b64dad730cb86f1ff7ac";
-  const example = `https://api.edamam.com/api/recipes/v2/0123456789abcdef0123456789abcdef?app_id=APP_ID&app_key=APP_KEY&type=public`;
+
+  //reloads content of the page at a specific instant
+  useEffect(() =>{
+    getRecipies();
+  }, []);
+
+  //Makes API call 
+  const getRecipies = async () =>{
+    const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
+    const data = await response.json();
+    console.log(data.hits);
+  };
 
   return (
     <div className="App">
